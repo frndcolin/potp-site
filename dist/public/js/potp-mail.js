c2c1c300b593c148842ddd1814c42a7afa8b74f3
-l <-l>
2017-12-27 02:52:48 -0500

    added validate js

--- dist/public/js/potp-mail.js
+++ dist/public/js/potp-mail.js
@@ -1,14 +1,15 @@
-(function(){
-    var formBtn         = document.querySelector('button[type="submit"]');
-    var formSuccess     = document.getElementById('form-success');
-    var formName        = document.querySelector('input#name');
-    var formEmail       = document.querySelector('input#email');
-    var formMsg         = document.querySelector('textarea#message');
-    var formInterest    = document.querySelector('select#interest');
-    
-    function sendForm(values){
+(function(w, d){
+    var form            = d.querySelector('form');
+    var formMsg     = d.getElementById('form-success');
+
+    var sendMail = function(e, values){
+        e.preventDefault();
+        console.log('sending');
+        
         emailjs.send("sendgrid","new_customer", values)
             .then(function(response) {
+            
+                form.style.display = 'none';
                 formMsg.style.display = 'block';
                 formMsg.innerHTML = '<h2>Message sent successfuly</h2>';
             }, function(err) {
@@ -16,60 +17,11 @@
             }
         );
     }
-    
-    function validateForm(e){
-        e.preventDefault();
-        let abc = /[a-z]/;
-        //assign form values to object to send via emailjs
-        var formValues = {
-            name     : validName(),
-            email    : validEmail(),
-            msg      : validMsg(),
-            service : formInterest.value
-        }
-        
-        //validate name
-        function validName(){
-            let name = formName.value;
-            if(name != '' && name.length <= 25 && abc.exec(name)){
-                return name;
-            } else {
-                alert("ERROR");
-            }
-            //ELSE SHOW ERR AND RESET
-        }
-        //validate name
-        function validEmail(){
-            let email = formEmail.value;
-            if(email != '' && email.length <= 25){
-                return email;
-            }
-            //ELSE SHOW ERR AND RESET
-        }
-        //validate name
-        function validMsg(){
-            let msg = formMsg.value;
-            if(msg != '' && msg.length <= 25){
-                return msg;
-            }
-            //ELSE SHOW ERR AND RESET
-        }
-//        
-//        //Test form for valid inputs
-//        if(formValues.name != '' && formValues.email != '' && formValues.msg != ''){
-//            //if form is valid hide form, show spinner, and send email
-//            document.querySelector('form').style.display = 'none';
-//            formMsg.innerHTML = '<h2><i class="fa fa-paw"></i></h2>';
-//            
-//        } else {
-//            formMsg.style.display = 'block';
-//            formMsg.innerHTML = '<h2>empty field</h2>';
-//            return false;
-//        }
-        
-//        sendForm(formValues);
-    }
-    
+    w.sendMail = sendMail;
     //listen for click on the submit and check values before sending form
-    formBtn.addEventListener('click', validateForm);
-}());
+//    form.addEventListener('validated', sendMail);
+}(window, document));
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = sendMail;
+}
