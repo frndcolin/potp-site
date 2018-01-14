de5588ff4bcbdb60a33ea594159e9c421cc33384
-l <-l>
2018-01-13 19:58:11 -0500

    final touches for live site

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,31 @@
 var gulp = require('gulp');
-var path = require('path');
-var watch = require('gulp-watch');
+let cleanCSS = require('gulp-clean-css');
+let sourcemaps = require('gulp-sourcemaps');
 var sitemap = require('gulp-sitemap');
-var concatCss = require('gulp-concat-css');
 
-
-gulp.task('watch-css', function () {
-    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event 
-    return watch('src/css/*.css', function () {
-        gulp.src('src/css/css-loader.css')
-            .pipe(concatCss("bundled.css"))
-            .pipe(gulp.dest('dist/public/css/'));
-    });
+gulp.task('minify-css',() => {
+  return gulp.src('./dist/public/css/bundled.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/public/css'));
 });
 
+//
+//var path = require('path');
+//var watch = require('gulp-watch');
+//var concatCss = require('gulp-concat-css');
+
+//
+//gulp.task('watch-css', function () {
+//    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event 
+//    return watch('src/css/*.css', function () {
+//        gulp.src('src/css/css-loader.css')
+//            .pipe(concatCss("bundled.css"))
+//            .pipe(gulp.dest('dist/public/css/'));
+//    });
+//});
+//
 gulp.task('sitemap', function () {
     gulp.src('dist/index.html', {
             read: false
